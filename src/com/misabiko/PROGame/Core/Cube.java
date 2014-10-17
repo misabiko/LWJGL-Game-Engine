7449ec5c5cd21e8b9942450546f981f7be188166
misabiko <misabiko@gmail.com>
2014-10-17 19:57:17 -0400

    Made cube vertices

--- src/com/misabiko/PROGame/Core/Cube.java
+++ src/com/misabiko/PROGame/Core/Cube.java
@@ -3,29 +3,58 @@ package com.misabiko.PROGame.Core;
 import java.nio.FloatBuffer;
 
 import org.lwjgl.BufferUtils;
-
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL15.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
 public class Cube {
 	
-	private float x,y,width,height;
+	public float x,y,z,width,height,depth;
 	private FloatBuffer verticesBuffer;
 	
-	public Cube(float x, float y, float w, float h) {
+	public Cube(float x, float y, float z, float w, float h, float d) {
 		this.x = x;
 		this.y = y;
+		this.z = z;
 		width = w;
 		height = h;
+		depth = d;
 		
 		float[] vertices = new float[] {
-				x,			y,			0.0f,
-				x+width,	y,			0.0f,
-				x+width,	y+height,	0.0f,
-				x,			y+height,	0.0f
+//				Front Face
+				x,			y,			z,
+				x+width,	y,			z,
+				x+width,	y+height,	z,
+				x,			y+height,	z,
+//				Back Face
+				x,			y,			z+depth,
+				x+width,	y,			z+depth,
+				x+width,	y+height,	z+depth,
+				x,			y+height,	z+depth,
+//				Left Face
+				x,			y,			z,
+				x,			y+height,	z,
+				x,			y+height,	z+depth,
+				x,			y,			z+depth,
+//				Right Face
+				x+width,	y,			z,
+				x+width,	y,			z+depth,
+				x+width,	y+height,	z+depth,
+				x+width,	y+height,	z,
+//				Bottom Face
+				x,			y,			z,
+				x+width,	y,			z,
+				x+width,	y,			z+depth,
+				x,			y,			z+depth,
+//				Top Face
+				x,			y+height,	z,
+				x+width,	y+height,	z,
+				x+width,	y+height,	z+depth,
+				x,			y+height,	z+depth,
 		};
 		
 		verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
 		verticesBuffer.put(vertices);
 		verticesBuffer.flip();
-		
-		
 	}
 }
