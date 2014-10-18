73327ccfb953933c87314a3b9a40d4dac2abd630
misabiko <misabiko@gmail.com>
2014-10-17 21:28:47 -0400

    Reduced the amount of vertices to 8, added an indice array and changed the vertex adding to element array

--- src/com/misabiko/PROGame/Core/Cube.java
+++ src/com/misabiko/PROGame/Core/Cube.java
@@ -1,5 +1,6 @@
 package com.misabiko.PROGame.Core;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
 import org.lwjgl.BufferUtils;
@@ -12,6 +13,8 @@ public class Cube {
 	
 	public float x,y,z,width,height,depth;
 	public FloatBuffer verticesBuffer;
+	public ByteBuffer indicesBuffer;
+	public int indicesCount;
 	
 	public Cube(float x, float y, float z, float w, float h, float d) {
 		this.x = x;
@@ -22,44 +25,49 @@ public class Cube {
 		depth = d;
 		
 		float[] vertices = new float[] {
+//				Vertices are set in a counterclockwise manner starting from bottom-left
 //				Front Face
 				x,			y,			z,
 				x+width,	y,			z,
-				x,			y+height,	z,
-				
-				x+width,	y,			z,
 				x+width,	y+height,	z,
 				x,			y+height,	z,
-////				Back Face
-//				x,			y,			z+depth,
-//				x+width,	y,			z+depth,
-//				x+width,	y+height,	z+depth,
-//				x,			y+height,	z+depth,
-////				Left Face
-//				x,			y,			z,
-//				x,			y+height,	z,
-//				x,			y+height,	z+depth,
-//				x,			y,			z+depth,
-////				Right Face
-//				x+width,	y,			z,
-//				x+width,	y,			z+depth,
-//				x+width,	y+height,	z+depth,
-//				x+width,	y+height,	z,
-////				Bottom Face
-//				x,			y,			z,
-//				x+width,	y,			z,
-//				x+width,	y,			z+depth,
-//				x,			y,			z+depth,
-////				Top Face
-//				x,			y+height,	z,
-//				x+width,	y+height,	z,
-//				x+width,	y+height,	z+depth,
-//				x,			y+height,	z+depth,
+//				Back Face
+				x,			y,			z+depth,
+				x+width,	y,			z+depth,
+				x+width,	y+height,	z+depth,
+				x,			y+height,	z+depth,
 		};
 		
+//		TODO Look further into Byte Buffers
 		verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
 		verticesBuffer.put(vertices);
 		verticesBuffer.flip();
+		
+		byte[] indices = {
+//			Sets the order in which the vertices should be used to produce triangles
+//			Front Face
+			0,1,3,
+			1,2,3,
+//			Back Face
+			4,5,7,
+			5,6,7,
+//			Left Face
+			0,4,3,
+			3,4,7,
+//			Right Face
+			1,5,2,
+			5,6,2,
+//			Bottom Face
+			0,1,4,
+			1,5,4,
+//			Top Face
+			3,2,7,
+			2,6,7
+		};
+		indicesCount = indices.length;
+		indicesBuffer = BufferUtils.createByteBuffer(indices.length);
+		indicesBuffer.put(indices);
+		indicesBuffer.flip();
 	}
 	
 	public void addVertices(int vaoId) {
@@ -69,15 +77,15 @@ public class Cube {
 //		Generate a Vertex Buffer Object
 		int vboId = glGenBuffers();
 		
-//		Bind the VBO and add the verticesBuffer to it
-		glBindBuffer(GL_ARRAY_BUFFER, vboId);
-		glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+//		Bind the VBO and add the indicesBuffer to it
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboId);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
 		
 //		Add the VBO in index 0 of the VAO, for every vertices there is 3 floats
 		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
 		
 //		Bind the VBO and the VAO to 0, or null (unbind)
-		glBindBuffer(GL_ARRAY_BUFFER,0);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,0);
 		glBindVertexArray(0);
 	}
 }
