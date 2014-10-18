414bf37597e24e3bec87a9a2cd247c6ffd58076d
misabiko <misabiko@gmail.com>
2014-10-18 18:25:10 -0400

    Removed colorsVertices and turned verticesBuffer into an interleaved buffer

--- src/com/misabiko/LWJGLGameEngine/Core/Cube.java
+++ src/com/misabiko/LWJGLGameEngine/Core/Cube.java
@@ -8,7 +8,7 @@ import org.lwjgl.BufferUtils;
 public class Cube {
 	
 	public Vertex[] vertices;
-	public FloatBuffer verticesBuffer, colorsBuffer;
+	public FloatBuffer verticesBuffer;
 	public ByteBuffer indicesBuffer;
 	public int indicesCount;
 	
@@ -17,25 +17,25 @@ public class Cube {
 		vertices = new Vertex[] {
 //				Vertices are set in a counterclockwise manner starting from bottom-left
 //				Front Face
-				new Vertex(x,	y,		z,		1f,0f,0f,1f),
-				new Vertex(x+w,	y,		z,		1f,0f,0f,1f),
-				new Vertex(x+w,	y+h,	z,		1f,0f,0f,1f),
-				new Vertex(x,	y+h,	z,		1f,0f,0f,1f),
+				new Vertex(x,	y,		z,		1f,	0f,	0f,	1f),
+				new Vertex(x+w,	y,		z,		1f,	0f,	0f,	1f),
+				new Vertex(x+w,	y+h,	z,		1f,	0f,	0f,	1f),
+				new Vertex(x,	y+h,	z,		1f,	0f,	0f,	1f),
 //				Back Face
-				new Vertex(x,	y,		z+d,	1f,0f,0f,1f),
-				new Vertex(x+w,	y,		z+d,	1f,0f,0f,1f),
-				new Vertex(x+w,	y+h,	z+d,	1f,0f,0f,1f),
-				new Vertex(x,	y+h,	z+d,	1f,0f,0f,1f)
+				new Vertex(x,	y,		z+d,	1f,	0f,	0f,	1f),
+				new Vertex(x+w,	y,		z+d,	1f,	0f,	0f,	1f),
+				new Vertex(x+w,	y+h,	z+d,	1f,	0f,	0f,	1f),
+				new Vertex(x,	y+h,	z+d,	1f,	0f,	0f,	1f)
 		};
 		
 //		TODO Look further into Byte Buffers
-		verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
-		verticesBuffer.put(vertices);
-		verticesBuffer.flip();
 		
-		colorsBuffer = BufferUtils.createFloatBuffer(colors.length);
-		colorsBuffer.put(colors);
-		colorsBuffer.flip();
+		verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
+		for (Vertex v : vertices) {
+			verticesBuffer.put(v.xyzw);
+			verticesBuffer.put(v.rgba);
+		}
+		verticesBuffer.flip();
 		
 		byte[] indices = {
 //			Sets the order in which the vertices should be used to produce triangles
