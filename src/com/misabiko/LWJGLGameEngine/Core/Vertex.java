000a6cb674b8c0a28303f55885967d05a80bbeb3
misabiko <misabiko@gmail.com>
2014-10-18 18:03:31 -0400

    Added W to the Vertex class

--- src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
+++ src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
@@ -1,10 +1,10 @@
 package com.misabiko.LWJGLGameEngine.Core;
 
 public class Vertex {
-	public float[] xyz,rgba;
+	public float[] xyzw,rgba;
 	
 	public Vertex(float x, float y, float z, float r, float g, float b, float a) {
-		xyz = new float[] {x,y,z};
+		xyzw = new float[] {x,y,z,1f};
 		rgba = new float[] {r,g,b,a};
  	}
 }
