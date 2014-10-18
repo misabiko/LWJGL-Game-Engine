ca9beb187980b51bf5b848da094590643eaeffcf
misabiko <misabiko@gmail.com>
2014-10-18 18:20:40 -0400

    Added the size in byte of the vertex class as a final int

--- src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
+++ src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
@@ -2,6 +2,8 @@ package com.misabiko.LWJGLGameEngine.Core;
 
 public class Vertex {
 	public float[] xyzw,rgba;
+//	A float is 4 bytes, and there are 8 floats in total, so 4*8=32
+	public static final int sizeInBytes = 32;
 	
 	public Vertex(float x, float y, float z, float r, float g, float b, float a) {
 		xyzw = new float[] {x,y,z,1f};
