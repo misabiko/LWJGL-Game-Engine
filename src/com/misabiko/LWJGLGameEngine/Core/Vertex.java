d120cfa6b71b2cc94d524ae9148de82044f5714e
misabiko <misabiko@gmail.com>
2014-10-18 21:08:37 -0400

    Added colorOffset and stOffset in byte

--- src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
+++ src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
@@ -5,8 +5,10 @@ public class Vertex {
 	public static final int posElementCount = 4;
 	public static final int colorElementCount = 4;
 	public static final int elementCount = posElementCount+colorElementCount;
-//	A float is 4 bytes
+	
 	public static final int bytesPerFloat = 4;
+	
+	public static final int colorOffset = bytesPerFloat*posElementCount;
 	
 	public Vertex(float x, float y, float z, float r, float g, float b, float a) {
 		xyzw = new float[] {x,y,z,1f};
