2f8b5db7c3597272eb235baf8e46812e0495599c
misabiko <misabiko@gmail.com>
2014-10-18 20:59:00 -0400

    Added the TexturedVertex class, subclass of Vertex adding texture support

---
+++ src/com/misabiko/LWJGLGameEngine/Core/TexturedVertex.java
@@ -0,0 +1,18 @@
+package com.misabiko.LWJGLGameEngine.Core;
+
+public class TexturedVertex extends Vertex{
+	
+	public float[] st;
+	public static final int stElementCount = 2;
+	public static final int elementCount = posElementCount+colorElementCount+stElementCount;
+	
+	public TexturedVertex(float x, float y, float z, float r, float g, float b, float a, float s, float t) {
+		super(x, y, z, r, g, b, a);
+		st = new float[] {s,t};
+	}
+	
+	public TexturedVertex(float x, float y, float z, float s, float t) {
+		this(x,y,z,1f,1f,1f,1f,s,t);
+ 	}
+
+}
