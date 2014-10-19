61f48f8f9f021edfe1d9fb3e121b5267c10855ec
misabiko <misabiko@gmail.com>
2014-10-18 20:48:43 -0400

    Added final ints for elementcount for position,color and st, also added a few additional constructors

--- src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
+++ src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
@@ -1,13 +1,27 @@
 package com.misabiko.LWJGLGameEngine.Core;
 
 public class Vertex {
-	public float[] xyzw,rgba;
-	public static final int elementCount = 8;
+	public float[] xyzw,rgba,st;
+	public static final int posElementCount = 4;
+	public static final int colorElementCount = 4;
+	public static final int stElementCount = 8;
+	public static final int elementCount = posElementCount+colorElementCount+stElementCount;
 //	A float is 4 bytes
-	public static final int sizeInBytes = 4*elementCount;
+	public static final int bytesPerFloat = 4;
+	
+	public Vertex(float x, float y, float z, float r, float g, float b, float a, float s, float t) {
+		xyzw = new float[] {x,y,z,1f};
+		rgba = new float[] {r,g,b,a};
+		st = new float[] {s,t};
+ 	}
 	
 	public Vertex(float x, float y, float z, float r, float g, float b, float a) {
 		xyzw = new float[] {x,y,z,1f};
 		rgba = new float[] {r,g,b,a};
+ 	}
+	
+	public Vertex(float x, float y, float z) {
+		xyzw = new float[] {x,y,z,1f};
+		rgba = new float[] {1f,1f,1f,1f};
  	}
 }
