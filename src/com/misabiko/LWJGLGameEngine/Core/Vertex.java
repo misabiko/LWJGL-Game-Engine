7af533647adf63db86eb2cd0450f392fbca0a2ef
misabiko <misabiko@gmail.com>
2014-10-18 20:53:14 -0400

    Removed the st (Gonna add a subclass for it)

--- src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
+++ src/com/misabiko/LWJGLGameEngine/Core/Vertex.java
@@ -1,19 +1,12 @@
 package com.misabiko.LWJGLGameEngine.Core;
 
 public class Vertex {
-	public float[] xyzw,rgba,st;
+	public float[] xyzw,rgba;
 	public static final int posElementCount = 4;
 	public static final int colorElementCount = 4;
-	public static final int stElementCount = 8;
-	public static final int elementCount = posElementCount+colorElementCount+stElementCount;
+	public static final int elementCount = posElementCount+colorElementCount;
 //	A float is 4 bytes
 	public static final int bytesPerFloat = 4;
-	
-	public Vertex(float x, float y, float z, float r, float g, float b, float a, float s, float t) {
-		xyzw = new float[] {x,y,z,1f};
-		rgba = new float[] {r,g,b,a};
-		st = new float[] {s,t};
- 	}
 	
 	public Vertex(float x, float y, float z, float r, float g, float b, float a) {
 		xyzw = new float[] {x,y,z,1f};
@@ -21,7 +14,6 @@ public class Vertex {
  	}
 	
 	public Vertex(float x, float y, float z) {
-		xyzw = new float[] {x,y,z,1f};
-		rgba = new float[] {1f,1f,1f,1f};
+		this(x,y,z,1f,1f,1f,1f);
  	}
 }
