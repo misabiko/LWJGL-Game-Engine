2203d6a211b8b3a66c0c128c8074883cd63a5965
misabiko <misabiko@gmail.com>
2014-10-19 05:34:50 -0400

    Added matrices to vertex shader

--- src/com/misabiko/LWJGLGameEngine/Shaders/vertex.glsl
+++ src/com/misabiko/LWJGLGameEngine/Shaders/vertex.glsl
@@ -1,5 +1,9 @@
 #version 150 core
 
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+
 in vec4 in_Position;
 in vec4 in_Color;
 in vec2 in_TextureCoord;
@@ -8,7 +12,7 @@ out vec4 pass_Color;
 out vec2 pass_TextureCoord;
 
 void main(void) {
-	gl_Position = in_Position;
+	gl_Position = projectionMatrix * viewMatrix * modelMatrix * in_Position;
 	
 	pass_Color = in_Color;
 	pass_TextureCoord = in_TextureCoord;
