a5e6d5246ad9ce6ccb59d8525ef15d113500672b
misabiko <misabiko@gmail.com>
2014-10-19 05:46:10 -0400

    Added Camera class (pretty much empty)

---
+++ src/com/misabiko/LWJGLGameEngine/Core/Camera.java
@@ -0,0 +1,11 @@
+package com.misabiko.LWJGLGameEngine.Core;
+
+import org.lwjgl.util.vector.Matrix4f;
+
+public class Camera {
+	Matrix4f viewMatrix = new Matrix4f();
+	
+	public Camera() {
+		
+	}
+}
