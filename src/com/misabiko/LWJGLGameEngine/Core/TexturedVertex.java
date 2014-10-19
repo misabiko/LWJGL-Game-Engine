fd2d3c53438562afe70d53c69ab782f0320a309f
misabiko <misabiko@gmail.com>
2014-10-18 23:47:43 -0400

    Overrided getElement method on TexturedVertex

--- src/com/misabiko/LWJGLGameEngine/Core/TexturedVertex.java
+++ src/com/misabiko/LWJGLGameEngine/Core/TexturedVertex.java
@@ -16,5 +16,22 @@ public class TexturedVertex extends Vertex{
 	public TexturedVertex(float x, float y, float z, float s, float t) {
 		this(x,y,z,1f,1f,1f,1f,s,t);
  	}
+	
+	public float[] getElements() {
+		return new float[] {
+				xyzw[0],
+				xyzw[1],
+				xyzw[2],
+				xyzw[3],
+				
+				rgba[0],
+				rgba[1],
+				rgba[2],
+				rgba[3],
+				
+				st[0],
+				st[1]
+		};
+	}
 
 }
