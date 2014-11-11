2ecfdbcee5219d24c3004007a08bbc63ff4376f4
misabiko <misabiko@gmail.com>
2014-11-11 11:20:31 -0500

    Added QUAD to enum and comments for each hitbox types

--- src/com/misabiko/LWJGLGameEngine/Physic/Hitbox.java
+++ src/com/misabiko/LWJGLGameEngine/Physic/Hitbox.java
@@ -1,5 +1,8 @@
 package com.misabiko.LWJGLGameEngine.Physic;
 
 public enum Hitbox {
-	CYLINDER,BOX,LINE
+	CYLINDER,	//Has a center xyz point, a height and a radius
+	BOX,		//Has a center xyz point, a height and a width
+	LINE,		//Has two xyz points
+	QUAD		//Has a center xyz point and a point on each corners
 }
