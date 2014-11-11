550057d37dda5627bec640954a96a233a4b6fbfe
misabiko <misabiko@gmail.com>
2014-11-11 11:06:57 -0500

    Added enum for hitbox types

--- src/com/misabiko/LWJGLGameEngine/Physic/CollisionDetection.java
+++ src/com/misabiko/LWJGLGameEngine/Physic/CollisionDetection.java
@@ -5,6 +5,9 @@ import org.lwjgl.util.vector.Vector3f;
 import com.misabiko.LWJGLGameEngine.Meshes.Mesh;
 
 public abstract class CollisionDetection {
+	public static enum HitboxType {
+		CYLINDER,BOX
+	}
 	
 	public static boolean collisionCheck(Mesh m1, Mesh m2) {
 		
