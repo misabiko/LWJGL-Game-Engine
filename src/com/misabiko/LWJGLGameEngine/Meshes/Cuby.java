20c19288e2c572c7443645250686c954a87f79b4
misabiko <misabiko@gmail.com>
2014-11-18 15:11:44 -0500

    Adapted physic to gameobject class

--- src/com/misabiko/LWJGLGameEngine/Meshes/Cuby.java
+++ src/com/misabiko/LWJGLGameEngine/Meshes/Cuby.java
@@ -2,26 +2,24 @@ package com.misabiko.LWJGLGameEngine.Meshes;
 
 import java.util.ArrayList;
 
+import com.misabiko.LWJGLGameEngine.GameObjects.GameObject;
 import com.misabiko.LWJGLGameEngine.Physic.Physic;
 
-public class Cuby extends Box{
-	
+public class Cuby extends GameObject {
+
 	private float jumpStrength = 0.05f;
 	public float speed = 0.005f;
-	public float hbRadius = 0.25f;
-	
+
 	public Cuby() {
-		super(0, 0, 0, 0.5f,0.5f,0.5f);
-		
-//		hitbox = Hitbox.CYLINDER;
+		super(0, 0, 0, new Box(0.5f,0.5f,0.5f));
 	}
-	
+
 	public void jump() {
 		yVel = jumpStrength;
 	}
-	
-	public void update(ArrayList<Mesh> meshes) {
-		Physic.update(this, meshes);
+
+	public void update(ArrayList<GameObject> objs) {
+		Physic.update(this, objs);
 		super.update();
 	}
 }
--- src/com/misabiko/LWJGLGameEngine/Physic/Physic.java
+++ src/com/misabiko/LWJGLGameEngine/Physic/Physic.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import org.lwjgl.util.vector.Vector3f;
 
-import com.misabiko.LWJGLGameEngine.Meshes.Mesh;
+import com.misabiko.LWJGLGameEngine.GameObjects.GameObject;
 
 public class Physic {
 	private static float ySpeedCap = -0.2f;
@@ -12,49 +12,48 @@ public class Physic {
 	private static float gravity = 0.05f;
 	private static float friction = 0.08f;
 	
-	private static void friction(Mesh m) {
-		if (m.xzVel.length() >= (friction/20)) {
-			m.xzVel.scale(1f-friction);
-		}else if (m.xzVel.length() <= (-friction/20)) {
-			m.xzVel.scale(1f-friction);
+	private static void friction(GameObject obj) {
+		if (obj.xzVel.length() >= (friction/20)) {
+			obj.xzVel.scale(1f-friction);
+		}else if (obj.xzVel.length() <= (-friction/20)) {
+			obj.xzVel.scale(1f-friction);
 		}else {
-			m.xzVel.scale(0);
+			obj.xzVel.scale(0);
 		}
 		
-		if (m.xzVel.length() > speedCap) {
-			m.xzVel.scale(speedCap/m.xzVel.length());
-		}else if (m.xzVel.length() < -speedCap) {
-			m.xzVel.scale(-speedCap/m.xzVel.length());
+		if (obj.xzVel.length() > speedCap) {
+			obj.xzVel.scale(speedCap/obj.xzVel.length());
+		}else if (obj.xzVel.length() < -speedCap) {
+			obj.xzVel.scale(-speedCap/obj.xzVel.length());
 		}
 	}
 	
-	private static void gravity(Mesh m) {
-		if (m.yVel > ySpeedCap)
-			m.yVel -= gravity;
+	private static void gravity(GameObject obj) {
+		if (obj.yVel > ySpeedCap)
+			obj.yVel -= gravity;
 		else
-			m.yVel = ySpeedCap;
+			obj.yVel = ySpeedCap;
 	}
 	
-	public static boolean isColliding(Mesh m1, Mesh m2) {
-		for (Vector3f sp : m1.hitbox.getSP()) {
-			if (m2.hitbox.isPointInside(sp))
+	public static boolean isColliding(GameObject obj1, GameObject obj2) {
+		for (Vector3f sp : obj1.hitbox.getSP()) {
+			if (obj1.hitbox.isPointInside(sp))
 				return true;
 		}
 		
-		for (Vector3f sp : m2.hitbox.getSP()) {
-			if (m1.hitbox.isPointInside(sp))
+		for (Vector3f sp : obj1.hitbox.getSP()) {
+			if (obj1.hitbox.isPointInside(sp))
 				return true;
 		}
 		
 		return false;
 	}
 	
-	public static void update(Mesh m, ArrayList<Mesh> meshes) {
-		
-		friction(m);
-		if (m.hitbox != null) {
-			for (Mesh mesh : meshes) {
-				if (isColliding(m, mesh) && mesh.hitbox != null)
+	public static void update(GameObject obj, ArrayList<GameObject> objs) {
+		friction(obj);
+		if (obj.hitbox != null) {
+			for (GameObject object : objs) {
+				if (isColliding(obj, object) && object.hitbox != null)
 					System.out.println("Collision");
 			}
 		}
