260ec6b2ea76968e79c37de06452f0a3f99bd7a0
misabiko <misabiko@gmail.com>
2015-01-09 15:15:55 -0500

    Adding the normal variable to the vertex shader

--- src/com/misabiko/LWJGLGameEngine/Shaders/vertex.glsl
+++ src/com/misabiko/LWJGLGameEngine/Shaders/vertex.glsl
@@ -7,13 +7,16 @@ uniform mat4 modelMatrix;
 in vec4 in_Position;
 in vec4 in_Color;
 in vec2 in_TextureCoord;
+in vec3 in_Normal;
 
 out vec4 pass_Color;
 out vec2 pass_TextureCoord;
+out vec3 pass_Normal;
 
 void main(void) {
 	gl_Position = projectionMatrix * viewMatrix * modelMatrix * in_Position;
 	
 	pass_Color = in_Color;
 	pass_TextureCoord = in_TextureCoord;
+	pass_Normal = in_Normal;
 }
