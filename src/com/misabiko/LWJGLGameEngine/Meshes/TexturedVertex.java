ca4ba5ad72152f1a158444a89bb0180bb61010eb
misabiko <misabiko@gmail.com>
2015-01-09 15:39:05 -0500

    Adding Normals to TexturedVertex

--- src/com/misabiko/LWJGLGameEngine/Meshes/TexturedVertex.java
+++ src/com/misabiko/LWJGLGameEngine/Meshes/TexturedVertex.java
@@ -2,26 +2,28 @@ package com.misabiko.LWJGLGameEngine.Meshes;
 
 import java.awt.Color;
 
+import org.lwjgl.util.vector.Vector3f;
+
 public class TexturedVertex extends Vertex{
 	
 	public float[] st;
 	public static final int stElementCount = 2;
-	public static final int elementCount = posElementCount+colorElementCount+stElementCount;
+	public static final int elementCount = posElementCount+colorElementCount+normElementCount+stElementCount;
 	
 	public static final int stOffset = (bytesPerFloat*posElementCount)+(bytesPerFloat*colorElementCount);
 	
-	public TexturedVertex(float x, float y, float z, float r, float g, float b, float a, float s, float t) {
-		super(x, y, z, r, g, b, a);
+	public TexturedVertex(float x, float y, float z, Vector3f n, float r, float g, float b, float a, float s, float t) {
+		super(x, y, z, n, r, g, b, a);
 		st = new float[] {s,t};
 	}
 	
-	public TexturedVertex(float x, float y, float z, Color color, float s, float t) {
-		super(x, y, z, color);
+	public TexturedVertex(float x, float y, float z, Vector3f n, Color color, float s, float t) {
+		super(x, y, z, n, color);
 		st = new float[] {s,t};
 	}
 	
-	public TexturedVertex(float x, float y, float z, float s, float t) {
-		this(x,y,z,1f,1f,1f,1f,s,t);
+	public TexturedVertex(float x, float y, float z, Vector3f n, float s, float t) {
+		this(x,y,z,n,1f,1f,1f,1f,s,t);
  	}
 	
 	public float[] getElements() {
@@ -37,7 +39,11 @@ public class TexturedVertex extends Vertex{
 				rgba[3],
 				
 				st[0],
-				st[1]
+				st[1],
+				
+				normal[0],
+				normal[1],
+				normal[2]
 		};
 	}
 
