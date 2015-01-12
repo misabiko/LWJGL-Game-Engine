01dd17ebd478b47424e44446ba3ca16514b3c18f
misabiko <misabiko@gmail.com>
2015-01-11 21:59:30 -0500

    Rem: angleZ on camera cause no one wants that Rem: rotateSpeed cause the camera follows the player, not the other way around. Ref: zoom to zoomSpeed cause yeah Signed-off-by: misabiko <misabiko@gmail.com>

--- src/com/misabiko/LWJGLGameEngine/Rendering/Camera.java
+++ src/com/misabiko/LWJGLGameEngine/Rendering/Camera.java
@@ -5,14 +5,13 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 	public static Matrix4f viewMatrix = new Matrix4f();
-	public static float angleY, angleX, angleZ = 0;
-	public static float rotateSpeed = 0.01f;
-	public static float zoom = 1f;
+	public static float angleY, angleX = 0;
+	public static float zoomSpeed = 1f;
 	
 	public static void update(Vector3f pos) {
 		Matrix4f.setIdentity(viewMatrix);
 		
-		Matrix4f.translate(new Vector3f(0,0,-zoom), viewMatrix, viewMatrix);
+		Matrix4f.translate(new Vector3f(0,0,-zoomSpeed), viewMatrix, viewMatrix);
 		
 		Matrix4f.rotate(-angleX, new Vector3f(1f,0,0), viewMatrix, viewMatrix);
 		Matrix4f.rotate(-angleY, new Vector3f(0,1f,0), viewMatrix, viewMatrix);
